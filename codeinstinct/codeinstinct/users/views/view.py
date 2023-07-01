dcd8cc9315a57713a70801e56b766c53b1202f9c
sachet <sachetgulia.com>
2023-07-01 11:31:18 +0530

    shift file

---
+++ codeinstinct/codeinstinct/users/views/view.py
@@ -0,0 +1,8 @@
+from django.views import View
+from django.shortcuts import render
+from codeinstinct.users.models.users import User
+
+class UserListView(View):
+    def get(self, request):
+        users = User.objects.all()
+        return render(request, 'user_list.html', {'users': users})
