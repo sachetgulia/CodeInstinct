6d566dfa9f9fc71bb00a66ab698c4b27742cbbe8
sachet <sachetgulia.com>
2023-07-15 04:36:50 +0530

    instinct engine

---
+++ codeinstinct/instinct_engine/__init__.py
@@ -0,0 +1,3 @@
+"""This Engine would be the centre of all the things happening for this project ,
+majorly involve all the AI and computations like the brains of this app and the reason
+ behind the name is that code_instinct it will work just like instincts works"""
--- codeinstinct/users/models/users.py
+++ codeinstinct/users/models/users.py
@@ -10,6 +10,7 @@ class User(models.Model):
     password = models.CharField(max_length=100, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
